590dcf011620e70329904dd007a3982fb6b7203d
Alexander Brasegård <alexbrasegard@gmail.com>
2026-01-27 18:17:39 +0100

    Added isAdmin function to auth.ts and styles to error messages

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -3,9 +3,18 @@
 import { LogIn } from "@/lib/auth";
 import Link from "next/link";
 import { LoaderCircle } from "lucide-react";
-import { useActionState } from "react";
+import { useActionState, useEffect, useState } from "react";
 const LoginForm = () => {
   const [state, action, pending] = useActionState(LogIn, {});
+  const [visible, setVisible] = useState(false);
+  useEffect(() => {
+    const showEror = async () => {
+      setVisible(true);
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      setVisible(false);
+    };
+    showEror();
+  }, [state]);
   return (
     <div className="w-[40vw] max-w-100 min-w-70 flex flex-col aspect-9/10 max-h-75 bg-foreground rounded-[20px] shadow-xl text-textPrimary relative">
       <div className="flex justify-center items-center w-full h-[25%] min-h-12">
@@ -32,18 +41,22 @@ const LoginForm = () => {
           required
           className="w-[80%] h-9.5 rounded-md bg-white px-3 mb-3 sm:h-11 focus:outline-0 shadow"
         />
-        <div className="w-full h-20 flex justify-center items-center">
+        <div className="w-full h-16 flex justify-center items-center">
           <button
             disabled={pending}
             type="submit"
-            className="flex justify-center items-center bg-buttons w-[40%] h-[50%] text-[20px] rounded-md cursor-pointer hover:bg-buttonsHover transition"
+            className="flex justify-center items-center bg-buttons w-[40%] h-[50%] min-h-10 text-[20px] rounded-md cursor-pointer hover:bg-buttonsHover transition"
           >
             {pending ? <LoaderCircle className="animate-spin" /> : "Log in"}
           </button>
         </div>
       </form>
-      <div className="flex items-center justify-center w-full h-10 text-destructive">
-        {state?.err}
+      <div className="flex items-center justify-center w-full h-15">
+        <p
+          className={`text-center text-destructive transition-opacity duration-600 ${visible ? "opacity-100" : "opacity-0"}`}
+        >
+          {state?.err}
+        </p>
       </div>
       <div className="absolute top-[calc(100%+10px)] text-textPrimary w-full flex justify-center gap-2 text-[14px] sm:text-[16px]">
         <p>Dont have an account?</p>
