68563d237e37fda38e1f916b230d1b73a2c64847
Hydrated1008 <axjo1008@gmail.com>
2026-01-31 15:00:45 +0100

    Fixed bug with SearchParams

--- components/bookings/DateDisplay.tsx
+++ components/bookings/DateDisplay.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-const DateDisplay = () => {
+const DateDisplayContent = () => {
   const searchParams = useSearchParams();
   const checkIn = searchParams.get("checkIn");
   const checkOut = searchParams.get("checkOut");
@@ -12,6 +13,14 @@ const DateDisplay = () => {
       <p>{checkIn}</p>
       <p>{checkOut}</p>
     </div>
+  );
+};
+
+const DateDisplay = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <DateDisplayContent />
+    </Suspense>
   );
 };
 
